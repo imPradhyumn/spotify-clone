acc08653fdf859c7a1e17f53fa81db1163f05e7b
imPradhyumn <pradhyumn.16jics040@jietjodhpur.ac.in>
2023-10-25 10:21:46 +0530

    Vercel build err resolved useEffect

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,8 @@ import ModalProvider from "@/providers/ModalProvider";
 import Providers from "@/redux/Provider";
 import SessionProvider from "@/providers/SessionProvider";
 import Player from "@/components/Player/Player";
-
+import { URL_PREFIX } from "@/constants";
+import axios from "axios";
 // import { setAuthState } from "./reducers/authSlice";
 
 const font = Figtree({ subsets: ["latin"] });
@@ -16,11 +17,22 @@ export const metadata: Metadata = {
   description: "An Spotify Clone",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  // Connect to DB as soon as land on search page
+
+  axios
+    .get(URL_PREFIX + "/search")
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
   return (
     <html lang="en">
       <body className={font.className}>
--- app/search/page.tsx
+++ app/search/page.tsx
@@ -2,23 +2,9 @@
 
 import Dashboard from "@/components/Search/Dashboard";
 import SearchBar from "@/components/Search/SearchBar";
-import { URL_PREFIX } from "@/constants";
-import axios from "axios";
-import React, { useEffect } from "react";
+import React from "react";
 
 const page = () => {
-  // Connect to DB as soon as land on search page
-  useEffect(() => {
-    axios
-      .get(URL_PREFIX + "/search")
-      .then((res) => {
-        console.log(res);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, []);
-
   return (
     <React.Fragment>
       <SearchBar />
