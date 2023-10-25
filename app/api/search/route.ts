9fc4512429cd78cc0cde5542259e5102edc51ecf
imPradhyumn <pradhyumn.16jics040@jietjodhpur.ac.in>
2023-10-25 11:07:24 +0530

    CORS added

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -4,8 +4,10 @@ import UserService from "@/services/userService";
 export async function GET(req: Request) {
   try {
     new UserService();
+    console.log({ res: "DB Connected", err: "" });
     return new Response(JSON.stringify({ res: "DB Connected", err: "" }));
   } catch (err) {
+    console.log({ res: "", err: `DB connection ERR : ${err}` });
     return new Response(
       JSON.stringify({ res: "", err: `DB connection ERR : ${err}` })
     );
--- next.config.js
+++ next.config.js
@@ -1,27 +1,29 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-module.exports = module.exports = {
-  webpack(config, options) {
-    const { isServer } = options;
-    config.module.rules.push({
-      test: /\.(ogg|mp3|wav|mpe?g)$/i,
-      exclude: config.exclude,
-      use: [
-        {
-          loader: require.resolve("url-loader"),
-          options: {
-            limit: config.inlineImageLimit,
-            fallback: require.resolve("file-loader"),
-            publicPath: `${config.assetPrefix}/_next/static/images/`,
-            outputPath: `${isServer ? "../" : ""}static/images/`,
-            name: "[name]-[hash].[ext]",
-            esModule: config.esModule || false,
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,DELETE,PATCH,POST,PUT",
           },
-        },
-      ],
-    });
-
-    return config;
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
   },
 };
+
+module.exports = nextConfig;
+
+module.exports = nextConfig;
