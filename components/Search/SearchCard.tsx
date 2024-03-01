d260f5c59238a2a41c6afefa0345be2b039d7026
imPradhyumn <pradhyumn.16jics040@jietjodhpur.ac.in>
2024-03-01 23:55:36 +0530

    Added/modified

--- components/Search/SearchCard.tsx
+++ components/Search/SearchCard.tsx
@@ -10,8 +10,8 @@ const SearchCard: React.FC<SearchCardProps> = ({ title, bgColor }) => {
   return (
     <div
       className={
-        "w-[12rem] h-[12rem]  rounded-lg justify-center cursor-pointer" +
-        ` bg-${bgColor}-500`
+        "w-[12rem] h-[12rem] rounded-lg justify-center cursor-pointer" +
+        ` bg-green-500`
       }
     >
       <h1 className="p-3 font-bold text-lg">{capitalize(title)}</h1>
